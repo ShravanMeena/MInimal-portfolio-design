10229135ee3d5f5aeed7b9a2e61ce7caf58cf3d9
Shravan <41790742+ShravanMeena@users.noreply.github.com>
2021-06-16 11:43:15 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../css/Footer.css";
 export default class Footer extends Component {
   render() {
     return (
-      <div className='footer'>© 2020 All rights reserved. by AamLadke</div>
+      <div className='footer'>© 2020 All rights reserved. by Pakkamarwadi</div>
     );
   }
 }
